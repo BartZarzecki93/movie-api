59c6b2ef14ba9f7a2b26e4d3cc4fdc623980bc4f
Bartosz Zarzecki <bart@parachut.co>
2021-01-13 15:06:54 +0100

    update workflows

--- src/test/db-handler.js
+++ src/test/db-handler.js
@@ -1,13 +1,11 @@
 const { connection } = require('mongoose');
 
 //Close the db and server.
-
 exports.closeDatabase = async () => {
 	await connection.close();
 };
 
 //Remove all the data for all db collections.
-
 exports.clearDatabase = async () => {
 	const collections = Object.keys(
 		connection.collections
